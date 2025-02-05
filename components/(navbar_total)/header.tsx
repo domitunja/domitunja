667def0b1423fc91f6fcaaf74e7324a5c657b696
jhoan34 <johanmonsalve125@gmail.com>
2025-02-05 09:29:45 -0500

    domitunja8888888

--- components/(navbar_total)/header.tsx
+++ components/(navbar_total)/header.tsx
@@ -1,10 +1,31 @@
+"use client"
 import { Navbar_mobile } from "./navbar_mobile/navbar_mobile";
 import { Navbar_desktop } from "./nav_desktop/nav_desktop";
 import "./header.css";
+import { useEffect, useState } from "react";
 
 export function Header() {
+    const [verm, setVerm] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setVerm(true);
+            } else {
+                setVerm(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        // Limpieza del event listener cuando el componente se desmonta
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []); // Se ejecuta solo una vez al montar el componente
+
     return (
-        <header className="fixed top-0 left-0 z-50 w-full h-[13vh] flex justify-center items-center">
+        <header className={`${verm ? "bg-black" : "bg-transparent"} fixed top-0 left-0 z-50 w-full h-[13vh] flex justify-center items-center transition-colors duration-300`}>
             <nav className="fade-in-up w-[77%] h-full flex justify-between flex-row items-center">
                 <div className="w-[30%] flex items-center justify-start h-full">
                     <img 
